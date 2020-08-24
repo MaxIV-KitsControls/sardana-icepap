5ec513a4381c51327dff3a90879ac147641a9d10
Abdullah <abdullah.amjad@maxiv.lu.se>
2020-08-24 15:12:34 +0200

    added simple setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="icepap_ctrl",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=["setuptools"],
+    description="IcePAP Sardana controller."
+)
