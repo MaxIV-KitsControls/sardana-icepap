e4ecad19dcef6b6cf25f9edf2222c1107b838891
Roberto Javier Homs Puron <rhoms@cells.es>
2020-08-28 11:20:33 +0200

    Add bump version configuration file

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
+# The version is updated automatically with bumpversion
+# Do not update manually
+__version = "0.1.0"
+
 
 setup(
     name="sardana_icepap",
-    version="1.0.0",
+    version=__version,
     packages=find_packages(),
     install_requires=["sardana", "icepap", "setuptools"],
     description="IcePAP Sardana controller."
