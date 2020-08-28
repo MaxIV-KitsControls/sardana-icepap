fa2f06e8040e9d42e4833180c590986098b49b21
Roberto Javier Homs Puron <rhoms@cells.es>
2020-08-28 11:10:49 +0200

    Change package name

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
+
 setup(
-    name="icepap_ctrl",
+    name="sardana_icepap",
     version="1.0.0",
     packages=find_packages(),
     install_requires=["setuptools"],
