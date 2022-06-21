f944c8991afe2a82ccd99d2b08e0a2e2d917bf19
Roberto Javier Homs Puron <rhoms@cells.es>
2022-06-21 15:09:19 +0200

    Bump version 1.0.6 to 1.1.0

--- sardana_icepap/__init__.py
+++ sardana_icepap/__init__.py
@@ -1,3 +1,3 @@
 # The version is updated automatically with bumpversion
 # Do not update manually
-__version__ = "1.0.6"
+__version__ = "1.1.0"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 # The version is updated automatically with bumpversion
 # Do not update manually
-__version = "1.0.6"
+__version = "1.1.0"
 
 
 setup(
