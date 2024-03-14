a1849f717b946fb453b569d82beb122e82d5a823
Oriol Vallcorba <ovallcorba@cells.es>
2024-03-14 11:31:24 +0100

    Bump version 2.5.2 to 2.5.3

--- sardana_icepap/__init__.py
+++ sardana_icepap/__init__.py
@@ -1,3 +1,3 @@
 # The version is updated automatically with bumpversion
 # Do not update manually
-__version__ = "2.5.2"
+__version__ = "2.5.3"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 # The version is updated automatically with bumpversion
 # Do not update manually
-__version = "2.5.2"
+__version = "2.5.3"
 
 
 setup(
